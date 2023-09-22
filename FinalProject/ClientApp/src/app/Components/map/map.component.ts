9980adc04fe82335068d6fe898e92c7e06ff1163
Tim Montague <tim.john.montague@gmail.com>
2023-09-22 16:11:42 -0400

    got to do maps for list

--- FinalProject/ClientApp/src/app/Components/map/map.component.ts
+++ FinalProject/ClientApp/src/app/Components/map/map.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { Coordinate } from 'src/app/Models/properties-by-postal';
+import { Coordinate, PropertiesByPostal } from 'src/app/Models/properties-by-postal';
 import { PropertyDetails } from 'src/app/Models/property-details';
 
 @Component({
@@ -9,100 +9,109 @@ import { PropertyDetails } from 'src/app/Models/property-details';
 })
 export class MapComponent {
 
-  @Input() favoritePins:PropertyDetails[] = [];
+  @Input() favoritePins: PropertyDetails[] = [];
+  @Input() listPins: PropertiesByPostal = {} as PropertiesByPostal;
   favoritePinResult: PropertyDetails = {} as PropertyDetails;
+  listPinResult:PropertiesByPostal = {} as PropertiesByPostal;
+  Lat: number = 0;
+  Lon: number = 0;
+  coordinates: Coordinate[] = [];
 
-  Lat:number = 0;
-  Lon:number = 0;
-  coordinates:Coordinate[] = [];
 
+  ngOnInit(): void {
+    if (this.favoritePinResult!=null){
+      this.GetCoordinates();
+    }
+    if (this.listPinResult!=null){
+      this.GetStartCoordinates();
+    }
 
-  ngOnInit():void{
-    this.GetCoordinates();
+    
     this.setupMap();
   }
 
-  // GetCoordinates():void{
-  //   this.favoritePins.forEach((c:PropertyDetails) => {
-  //     this.coordinates.push(c.data.location.address.coordinate)
-  //   })
-  // }
 
-  // GetCoordinates():void{
-  //   console.log('Get Coordinates')
-  //   console.log(this.favoritePins);
-  //   // for (let index = 0; index < this.favoritePins.length; index++) {
-  //   //   this.coordinates[index].lat = (Number(this.favoritePins[index].data.location.address.coordinate.lat));
-  //   //   this.coordinates[index].lon = (Number(this.favoritePins[index].data.location.address.coordinate.lon));
-  //   // }
-  //   this.favoritePins.forEach((p) => {
-  //     if (p.data.location.address.coordinate.lat != null)
-  //     {
-  //     let lat = (Number(p.data.location.address.coordinate.lat));
-  //     let lon = (Number(p.data.location.address.coordinate.lon));
-  //     let coord:Coordinate = {} as Coordinate;
-  //     coord.lat = lat;
-  //     coord.lon = lon;
-  //     this.coordinates.push(coord);
-  //     }
-  //   })
-  //   console.log(this.coordinates)
-  // }
 
-  GetCoordinates():Coordinate[]{
+  GetCoordinates(): Coordinate[] {
     // console.log('Get Coordinates')
     // console.log(this.favoritePins);
-    let cords:Coordinate[] = [];
+    let cords: Coordinate[] = [];
     this.favoritePins.forEach((p) => {
-      if (p.data.location.address.coordinate.lat != null)
-      {
-      let lat = (Number(p.data.location.address.coordinate.lat));
-      let lon = (Number(p.data.location.address.coordinate.lon));
-      let coord:Coordinate = {} as Coordinate;
-      coord.lat = lat;
-      coord.lon = lon;
-      cords.push(coord);
+      if (p.data.location.address.coordinate.lat != null) {
+        let lat = (Number(p.data.location.address.coordinate.lat));
+        let lon = (Number(p.data.location.address.coordinate.lon));
+        let coord: Coordinate = {} as Coordinate;
+        coord.lat = lat;
+        coord.lon = lon;
+        cords.push(coord);
+      }
+    })
+    // console.log(this.coordinates)
+    return cords;
+  }
+  /////
+  GetStartCoordinates(): Coordinate[] {
+    // console.log('Get Coordinates')
+    // console.log(this.favoritePins);
+    let cords: Coordinate[] = [];
+    this.listPins.data.home_search.results.forEach((p) => {
+      if (p.location.address.coordinate != null) {
+        let lat = (Number(p.location.address.coordinate.lat));
+        let lon = (Number(p.location.address.coordinate.lon));
+        let coord: Coordinate = {} as Coordinate;
+        coord.lat = lat;
+        coord.lon = lon;
+        cords.push(coord);
       }
     })
     // console.log(this.coordinates)
     return cords;
   }
 
-  Click(){
+
+  Click() {
     console.log("mapClicked")
   }
-  
-  
+
+
   display: any;
-    center: google.maps.LatLngLiteral = {} as google.maps.LatLngLiteral;
+  center: google.maps.LatLngLiteral = {} as google.maps.LatLngLiteral;
   zoom = 7;
   
-  setupMap():void {
-    this.favoritePinResult = this.favoritePins[0];
-    this.Lat=Number(this.favoritePinResult.data.location.address.coordinate.lat);
-    this.Lon=Number(this.favoritePinResult.data.location.address.coordinate.lon);
-  
-  this.center = {
-    lat: this.Lat,
-    lng: this.Lon
-  };
+  setupMap(): void {
+    if (this.favoritePinResult != null) {
+      this.favoritePinResult = this.favoritePins[0];
+      this.Lat = Number(this.favoritePinResult.data.location.address.coordinate.lat);
+      this.Lon = Number(this.favoritePinResult.data.location.address.coordinate.lon);
+    }
+
+    if (this.listPinResult != null) {
+      this.listPinResult = this.listPins;
+      this.Lat = Number(this.listPinResult.data.home_search.results[0].location.address.coordinate?.lat);
+      this.Lon = Number(this.listPinResult.data.home_search.results[0].location.address.coordinate?.lon);
+    }
+    
+    this.center = {
+      lat: this.Lat,
+      lng: this.Lon
+    };
   }
-  
+
   /*------------------------------------------
   --------------------------------------------
   moveMap()
   --------------------------------------------
   --------------------------------------------*/
   moveMap(event: google.maps.MapMouseEvent) {
-      if (event.latLng != null) this.center = (event.latLng.toJSON());
+    if (event.latLng != null) this.center = (event.latLng.toJSON());
   }
-  
+
   /*------------------------------------------
   --------------------------------------------
   move()
   --------------------------------------------
   --------------------------------------------*/
   move(event: google.maps.MapMouseEvent) {
-      if (event.latLng != null) this.display = event.latLng.toJSON();
+    if (event.latLng != null) this.display = event.latLng.toJSON();
   }
 }
